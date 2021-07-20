6415337ce720590d1314175596c1bb439ac72b77
hks_74123 <56163188+hks74123@users.noreply.github.com>
2021-07-20 20:02:00 +0530

    Update settings.py

--- fbclone/settings.py
+++ fbclone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s9ur5z0w#&_c6@(ybcb1s$5(!p9y^6eto((4-w#%@o^t$=_t@z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','hks-fbclone.herokuapp.com']
 
