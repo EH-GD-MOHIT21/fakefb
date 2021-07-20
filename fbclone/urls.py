d1f096f2c054299b2777afefb61caf0a5cac50a2
hks_74123 <56163188+hks74123@users.noreply.github.com>
2021-07-20 21:03:23 +0530

    Update urls.py

--- fbclone/urls.py
+++ fbclone/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import url
 from django.views.static import serve
 from django.conf import settings
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('meramaddatumguddu/', admin.site.urls),
     path('',include('hksfbclone.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
 ]
